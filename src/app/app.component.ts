7cb0f272eba1e3d1459cb14420b8d5746e835baf
RA9 <cnah27@gmail.com>
2019-10-23 10:11:37 +0000

    Refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AppService} from './app.service';
 
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {NavBarComponent} from "../navbar/navbar.component";
 import {AppService} from './app.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, NavBarComponent ],
   providers: [AppService],
   bootstrap:    [ AppComponent ]
 })
